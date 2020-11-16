a06ded30138cc134d4048dca49db7d84c6892eb3
ywrk <ywrk.wprg@gmail.com>
2020-11-16 17:57:51 +0900

    デバイス関連のビューやpathの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,27 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
       with_options presence: true do
-      validates :nickname
-      validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "全角で記入してください"}
-      validates :last_name , format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "全角で記入してください"}
-      validates :first_name_kana, format: { with: /\A[ァ-ン]+\z/, message: "全角カナで記入してください"}
-      validates :last_name_kana , format: { with: /\A[ァ-ン]+\z/, message: "全角カナで記入してください"}
-      validates :birthday
+        VALID_PASSWORD_REGEX =/\A(?=.*?[a-zA-Z])(?=.*?[\d])\w{6,}\z/
+        validates :nickname
+        validates :password  , format: { with: VALID_PASSWORD_REGEX  , message: "半角6文字以上で英字と数字の両方を含めて設定してください"}
+        validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "全角で記入してください"}
+        validates :last_name , format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "全角で記入してください"}
+        validates :first_name_kana, format: { with: /\A[ァ-ン]+\z/, message: "全角カナで記入してください"}
+        validates :last_name_kana , format: { with: /\A[ァ-ン]+\z/, message: "全角カナで記入してください"}
+        validates :birthday
       end
+
+      # validates :nickname, presence: true
+      # validates :first_name, presence: true
+      # validates :last_name , presence: true
+      # validates :first_name_kana, presence: true
+      # validates :last_name_kana , presence: true
+      # validates :birthday , presence: true
+      
+
       
          
 end
