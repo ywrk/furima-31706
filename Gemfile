040e75129cec92f80c28c2ce762be890b8d9790a
ywrk <ywrk.wprg@gmail.com>
2020-11-18 19:27:58 +0900

    Active Storageの導入

--- Gemfile
+++ Gemfile
@@ -66,3 +66,5 @@ end
 
 gem 'devise'
 
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,12 @@
 class Item < ApplicationRecord
 
   belongs_to :user
+  has_one_attached :image
   
+
+  private
+  def item_params
+    params.require(:item).permit(:image).merge(user_id: current_user.id)
+  end
+
 end
