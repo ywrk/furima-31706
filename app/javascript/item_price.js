8589fcd083532e7556e029c9241b1cdc43beb5d0
ywrk <ywrk.wprg@gmail.com>
2020-11-20 18:29:23 +0900

    販売手数料や販売利益の表示

---
+++ app/javascript/item_price.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', () => {
+   const priceInput = document.getElementById("item-price");
+   priceInput.addEventListener("input", () => {
+   const inputValue = priceInput.value;
+   console.log(inputValue);
+   const addTaxDom = document.getElementById("add-tax-price");
+   addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
+   console.log(addTaxDom.innerHTML);
+   const profit = document.getElementById("profit");
+   profit.innerHTML = Math.floor(inputValue - addTaxDom.innerHTML);
+   console.log(profit.innerHTML);
+   })
+
+ });
