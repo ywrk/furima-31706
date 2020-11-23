66db5e033c347a6b3362fa5544d902778fd8fbae
ywrk <ywrk.wprg@gmail.com>
2020-11-23 14:43:56 +0900

    コントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -20,23 +21,24 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
-       redirect_to item_path(item.id)
+      render :show
     else
       render :edit
     end
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   def item_params
     params.require(:item).permit(:image, :title, :price, :description, :category_id, :condition_id, :shipping_from_id, :shipping_fee_id, :estimated_shipping_id).merge(user_id: current_user.id)
